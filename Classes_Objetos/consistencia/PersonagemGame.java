39d8722b01bedcdcec69324d49eeac359ca482a4
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-07-16 11:52:52 -0300

    Consistência

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,51 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeInicial, String nomeInicial) {
+        setSaudeAtual(saudeInicial);
+        setNome(nomeInicial);
+    }
+
+    public int getSaudeAtual(){
+        return saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        if(nome != null && !nome.trim().isEmpty())
+            this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(this.saudeAtual - quantidadeDeDano);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setSaudeAtual(this.saudeAtual + quantidadeDeCura);
+    }
+
+    public void setSaudeAtual(int valor) {
+        if (valor < 0) {
+            valor = 0;
+        } else if (valor > 100) {
+            valor = 100;
+        }
+
+        this.saudeAtual = valor;
+
+        if (valor > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+}
