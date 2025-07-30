13e20e219e18f1ac88cb10168dfb66ea3dd33cfa
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-07-30 18:24:30 -0300

    Arquivo

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWritingExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            System.out.println("Digite o texto para salvar no arquivo. Digite 'sair' para finalizar.");
+
+            while (true) {
+                String linha = scanner.nextLine();
+                if (linha.equalsIgnoreCase("sair")) {
+                    break;
+                }
+                writer.write(linha);
+                writer.newLine();
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Erro ao criar ou escrever no arquivo");
+        }
+
+        scanner.close();
+    }
+}
