7d23a666ec0f6e88acbf8c86d3bd32ece4e1821f
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-08-05 20:23:57 -0300

    Arquivo 2 correcao

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -6,6 +6,9 @@ public class FileReadingExercise {
     public static void main(String[] args) {
         String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
 
+        System.out.println("Conteúdo do arquivo '" + fileName + "':");
+        System.out.println();
+
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String linha;
             while ((linha = reader.readLine()) != null) {
@@ -13,6 +16,9 @@ public class FileReadingExercise {
             }
         } catch (IOException e) {
             System.out.println("Erro de leitura do arquivo");
+        } finally {
+            System.out.println();
+            System.out.println("Leitura do arquivo concluída.");
         }
     }
 }
