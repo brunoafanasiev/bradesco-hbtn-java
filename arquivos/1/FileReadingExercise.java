634cea4d293f772727a0be787ed5871643cae37d
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-08-05 20:18:41 -0300

    Arquivo 2

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro de leitura do arquivo");
+        }
+    }
+}
